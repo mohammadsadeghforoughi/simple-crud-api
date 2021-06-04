233dd303f03a7b6f1951d4eaba22ce1f7ffa358f
Mohammad Sadegh Foroughi <mohamad.foroughi@yahoo.com>
2021-06-04 11:48:27 +0430

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,13 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(cors())
+var options = {
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+}
+app.use(cors(options));
 
 app.use(logger("dev"));
 app.use(express.json());
